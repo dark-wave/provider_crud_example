056d93333217439cc9c8383daa4e7e71b96c1c57
dark-wave <noe.montes.developer@gmail.com>
2022-06-27 23:46:40 +0200

    Actualzación de estilos en User Form

--- lib/src/pages/user_form.dart
+++ lib/src/pages/user_form.dart
@@ -30,65 +30,76 @@ class _UserFormPageState extends State<UserFormPage> {
       appBar: AppBar(
         title: const Text('New User'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextFormField(
-              controller: _inputNameController,
-              decoration: const InputDecoration(
-                hintText: 'Introduce nombre',
-                labelText: 'Nombre'
-              ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return 'El nombre es obligatorio';
-                }
-
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _inputLastNameController,
-              decoration: const InputDecoration(
-                hintText: 'Introduce apellidos',
-                labelText: 'Apellidos'
-              ),
-            ),
-            TextFormField(
-              controller: _inputEmailController,
-              decoration: const InputDecoration(
-                hintText: 'Introduce email',
-                labelText: 'Email'
-              ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return 'El email es obligatorio';
-                }
-
-                bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
-                if(!emailValid){
-                  return 'El formato del email no es válido';
-                }
-                
-                return null;
-              },
-            ),
-            Expanded(child: Container()),
-            ElevatedButton(
-              child: const Text('Agregar'),
-              onPressed: (){
-                if(_formKey.currentState!.validate()){
-                  final userService = Provider.of<UserProvider>(context, listen: false);
-                  userService.addUser(_inputNameController.text, _inputLastNameController.text, _inputEmailController.text);
-
-                  Navigator.of(context).pop();
-                }
-              }
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _inputNameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Introduce nombre',
+                    labelText: 'Nombre'
+                  ),
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'El nombre es obligatorio';
+                    }
+      
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _inputLastNameController,
+                  decoration: const InputDecoration(
+                    hintText: 'Introduce apellidos',
+                    labelText: 'Apellidos'
+                  ),
+                ),
+                TextFormField(
+                  controller: _inputEmailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Introduce email',
+                    labelText: 'Email'
+                  ),
+                  validator: (value){
+                    if(value!.isEmpty){
+                      return 'El email es obligatorio';
+                    }
+      
+                    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
+                    if(!emailValid){
+                      return 'El formato del email no es válido';
+                    }
+                    
+                    return null;
+                  },
+                ),
+                Expanded(child: Container()),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(40),
+                  ),
+                  onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                      final userService = Provider.of<UserProvider>(context, listen: false);
+                      userService.addUser(_inputNameController.text, _inputLastNameController.text, _inputEmailController.text);
+      
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: const Text(
+                    'Agregar',
+                    style: TextStyle(fontSize: 20.0),
+                  )
+                )
+              ],
             )
-          ],
-        )
+          ),
+        ),
       )
     );
   }
