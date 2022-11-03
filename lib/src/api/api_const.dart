7aca82b205ef2ecc5b29d5fd9b3c972320d024ac
dark-wave <noe.montes.developer@gmail.com>
2022-11-03 23:08:05 +0100

    Definicion Api provider y configuracion base url

---
+++ lib/src/api/api_const.dart
@@ -0,0 +1,4 @@
+class ApiConst{
+  static const baseUrlAndroid = 'http://127.0.0.1:9090/';
+  static const baseUrlIos = 'http://localhost:8080/';
+}
--- lib/src/provider/user_api_provider.dart
+++ lib/src/provider/user_api_provider.dart
@@ -5,4 +5,12 @@ class UserApiProvider extends ChangeNotifier{
   static List<User> _userList = []; 
 
   List<User> get userList => _userList;
+
+  Future<void> addUser(String nombre, String apellidos, String email) async{}
+
+  Future<void> updateUser(User user) async{}
+
+  Future<void> deleteUser(int id) async{}
+
+  Future<void> listUsers() async{}
 }
